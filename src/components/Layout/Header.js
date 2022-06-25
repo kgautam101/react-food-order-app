6161adb6754c2f615ef6b7dcad94064133d8399b
Kaustubh Gautam <kaustubh.gautam@publicissapient.com>
2022-06-26 00:13:18 +0530

    meals summary and components 1 added

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,12 +2,28 @@ import React from "react";
 import mealsImage from "../../assets/meals.jpg";
 import classes from "./Header.module.css";
 import HeaderCartButton from "./HeaderCartButton";
+import homeSVG from "../../assets/home-svgrepo-com.svg";
+import foodSVG from "../../assets/fast-food-svgrepo-com.svg";
 
 const Header = (props) => {
   return (
     <React.Fragment>
       <header className={classes.header}>
-        <h1>Order2Home</h1>
+        <div className={classes.appLogo} onClick={() => {}}>
+          <img
+            className={classes.svgIcons}
+            src={foodSVG}
+            alt="food
+           Icon"
+          />
+          <div className={classes.Logotitle}>
+            <h3>Food</h3>
+            <h1 style={{ color: "#f13c20" }}>2</h1>
+            <h3>Home</h3>
+          </div>
+
+          <img className={classes.svgIcons} src={homeSVG} alt="home Icon" />
+        </div>
         <HeaderCartButton />
       </header>
       <div className={classes["main-image"]}>
